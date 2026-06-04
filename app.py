3ce4f89fc3e4510d90c3550f5100924642032e88
Jitenar <jitendar.deshna@gmail.com>
2026-06-04 16:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    topics = [
+        "History of Machine Learning",
+        "Learning Roadmap",
+        "Data Preprocessing",
+        "Train/Test Split",
+        "Feature Scaling",
+        "Linear Regression",
+        "Decision Trees",
+        "Random Forest",
+        "Voting Classifier",
+        "TPOT AutoML"
+    ]
+    return render_template("index.html", topics=topics)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
